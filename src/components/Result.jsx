27bce7566c946bffda2d21f25fdc755b98c8a357
SD-IITKGP <jaihindsourabh@kgpian.iitkgp.ac.in>
2023-07-02 22:34:29 +0530

    removed some errors from result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
 
 export default function Result() {
   const [tenderId, setTenderId] = useState(0);
   const [tokenId, setTokenId] = useState(0);
-  const [name, setName] = useState(".....");
+  const [name, setName] = useState(" [Getting Data.....] ");
+  const [visible, setVisible] = useState("none");
   // const [, set] = useState("...");
   // const [, set] = useState("...");
   // const [, set] = useState("...");
@@ -30,25 +33,30 @@ export default function Result() {
 
   useEffect(() => {
     async function chk1(){
+      setVisible("block");
       await axios
       .get("https://tenderrelease.cyclic.app/tenderDetails/" + tokenId)
       .then((response) => {
         if (response.data === "Error!!") {
           alert("Error");
+          setVisible("none");
         } else {
           console.log(response.data);
-          setName(response.data.companyName);
-          setRows( [
-          { id: 1, Field: "Company Name", Value: response.data.companyName},
-          { id: 2, Field: "Token Id", Value: response.data.tokenId},
-          { id: 3, Field: "Owner Name", Value: response.data.ownerName},
-          { id: 4, Field: "Email", Value: response.data.email},
-          { id: 5, Field: "DIN Number", Value: response.data.din},
-          { id: 6, Field: "Phone Number", Value: response.data.phoneNumber},
-          { id: 7, Field: "Tender Id", Value: response.data.tenderId},
-          { id: 8, Field: "Transaction Hash", Value: response.data.transactionHash},
-          { id: 9, Field: "Bid Amount", Value: response.data.bid}
-          ])
+          if(tenderId!==0&&tokenId!==0){
+            setName(response.data.companyName);
+            setRows( [
+            { id: 1, Field: "Company Name", Value: response.data.companyName},
+            { id: 2, Field: "Token Id", Value: response.data.tokenId},
+            { id: 3, Field: "Owner Name", Value: response.data.ownerName},
+            { id: 4, Field: "Email", Value: response.data.email},
+            { id: 5, Field: "DIN Number", Value: response.data.din},
+            { id: 6, Field: "Phone Number", Value: response.data.phoneNumber},
+            { id: 7, Field: "Tender Id", Value: response.data.tenderId},
+            { id: 8, Field: "Transaction Hash", Value: response.data.transactionHash},
+            { id: 9, Field: "Bid Amount", Value: response.data.bid}
+            ])
+            setVisible("none");
+          }
         }
       });
     }
@@ -74,7 +82,8 @@ export default function Result() {
   console.log(rows);
   return(
     <div>
-    <div className="containers mx-auto text-5xl font-semibold  my-48 text-center">And the winner is company: {name}, for the Tender Id: {tenderId}</div>
+      <div className="containers mx-auto text-5xl font-semibold  my-20 text-center">And the winner is company: {name}, for the Tender Id: {tenderId}</div>
+      <LinearProgress className="mx-60 my-20" style={{ display: visible}} color="success"  />
     <Box className="px-10" sx={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={rows}
